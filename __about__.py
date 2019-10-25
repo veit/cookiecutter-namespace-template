caf43c4961b61a511de2615600104c45bedd0676
veit <kontakt@veit-schiele.de>
2019-10-25 20:06:29 +0200

    Make setup.py a boilerplate

---
+++ __about__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "cookiecutter-namespace-template"
+__summary__ = "Cookiecutter template for a Python namespace package"
+__url__ = "https://github.com/veit/cookiecutter-namespace-template"
+
+__version__ = "0.1.8"
+
+__author__ = "Veit Schiele"
+__email__ = "contact@veit-schiele.de"
+
+__license__ = "BSD"
+__copyright__ = "Copyright 2017–2019 {}".format(__author__)
+
--- setup.py
+++ setup.py
@@ -1,38 +1,58 @@
 # !/usr/bin/env python
 
+import codecs
+import os
+import re
+
+from setuptools import setup
+
+###################################################################
+
+PACKAGES = []
+META_PATH = os.path.join(__about__.py")
+KEYWORDS = ['cookiecutter', 'template', 'package', ]
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development',
+]
+INSTALL_REQUIRES = []
+
+###################################################################
+
+about = {}
+with open(os.path.join("__about__.py")) as f:
+    exec(f.read(), about)
+
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 from distutils.core import setup
 setup(
-    name='cookiecutter-namespace-template',
-    packages=[],
-    version='0.1.8',
-    description='Cookiecutter template for a Python namespace package',
+    name=about["__title__"],
+    packages=PACKAGES,
+    version=about["__version__"],
+    description=about["__summary__"],
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    author='Veit Schiele',
-    license='BSD',
-    author_email='contact@veit-schiele.de',
-    url='https://github.com/veit/cookiecutter-namespace-template',
-    keywords=['cookiecutter', 'template', 'package', ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development',
-    ],
+    author=about["__author__"],
+    license=about["__license__"],
+    author_email=about["__email__"],
+    url=about["__url__"],
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
 )
