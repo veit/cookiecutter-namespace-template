a7aff91fead9fd77bce143c6dd38193fee0e2fdb
veit <kontakt@veit-schiele.de>
2019-10-25 21:22:27 +0200

    Make setup.py in template a boilerplate

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -3,7 +3,52 @@
 
 """The setup script."""
 
+import codecs
+import os
+import re
+
 from setuptools import setup, find_packages
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Other/Proprietary License': 'License :: Other/Proprietary License'
+} %}
+
+###################################################################
+
+PACKAGES = []
+META_PATH = os.path.join("{{cookiecutter.namespace}}", "__about__.py")
+
+KEYWORDS = ['{{ cookiecutter.project_slug }}', ]
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+{%- if cookiecutter.license in license_classifiers %}
+    '{{ license_classifiers[cookiecutter.license] }}',
+{%- endif %}
+{%- if cookiecutter.license == 'Other/Proprietary License' %}
+    'Private :: Do Not Upload',
+{%- endif %}
+    'Natural Language :: English',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+]
+
+###################################################################
+
+about = {}
+with open(os.path.join("__about__.py")) as f:
+    exec(f.read(), about)
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -17,58 +62,32 @@ setup_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest-runner',{%
 
 test_requirements = [{%- if cookiecutter.use_pytest == 'y' %}'pytest',{%- endif %} ]
 
-{%- set license_classifiers = {
-    'MIT license': 'License :: OSI Approved :: MIT License',
-    'BSD license': 'License :: OSI Approved :: BSD License',
-    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
-    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
-    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Other/Proprietary License': 'License :: Other/Proprietary License'
-} %}
-
 setup(
-    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
-    author_email='{{ cookiecutter.email }}',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-{%- if cookiecutter.license in license_classifiers %}
-        '{{ license_classifiers[cookiecutter.license] }}',
-{%- endif %}
-{%- if cookiecutter.license == 'Other/Proprietary License' %}
-        'Private :: Do Not Upload',
-{%- endif %}
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    description="{{ cookiecutter.project_short_description }}",
+    name=about["__title__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    version=about["__version__"],
+    description=about["__summary__"],
+    long_description=readme + '\n\n' + history,
+    long_description_content_type="text/x-rst",
+    {%- if cookiecutter.license in license_classifiers %}
+        license=about["__license__"],
+    {%- endif %}
+    url=about["__url__"],
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
     {%- if 'no' not in cookiecutter.command_line_interface|lower %}
-    entry_points={
-        'console_scripts': [
-            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
-        ],
-    },
+        entry_points={
+            'console_scripts': [
+                '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main',
+            ],
+        },
     {%- endif %}
     install_requires=requirements,
-{%- if cookiecutter.license in license_classifiers %}
-    license="{{ cookiecutter.license }}",
-{%- endif %}
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='{{ cookiecutter.project_slug }}',
-    name='{{ cookiecutter.project_slug }}',
     packages=find_packages(include=['{{ cookiecutter.project_slug }}']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
-    version='{{ cookiecutter.version }}',
     zip_safe=False,
 )
