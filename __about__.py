52edcb83cd6a400f0522acb6d595bebe6f48e17a
veit <kontakt@veit-schiele.de>
2019-10-27 09:50:21 +0100

    Update to 0.1.12

--- __about__.py
+++ __about__.py
@@ -7,7 +7,7 @@ __title__ = "cookiecutter-namespace-template"
 __summary__ = "Cookiecutter template for a Python namespace package"
 __url__ = "https://github.com/veit/cookiecutter-namespace-template"
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 
 __author__ = "Veit Schiele"
 __email__ = "contact@veit-schiele.de"
