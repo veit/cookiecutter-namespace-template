e8d7b5cdc02711d24ee2b1f3759f6c5398245fbd
veit <kontakt@veit-schiele.de>
2019-11-27 00:01:49 +0100

    Fix test_using_unittest

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -192,7 +192,7 @@ def test_using_pytest(cookies):
 
 
 def test_using_unittest(cookies):
-    with bake_in_temp_dir(cookies) as result:
+    with bake_in_temp_dir(cookies, extra_context={'use_pytest': 'n'}) as result:
         assert result.project.isdir()
         test_file_path = result.project.join(
             'tests/test_vsc-example.py'
