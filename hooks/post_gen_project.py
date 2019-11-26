bf0ec1c1f08af21b22179687aff19b299eae0d14
veit <kontakt@veit-schiele.de>
2019-11-26 17:19:43 +0100

    Switch to remove_package_file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -19,16 +19,16 @@ def remove_package_file(filepath):
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
-        remove_file('AUTHORS.rst')
-        remove_file('docs/authors.rst')
+        remove_project_file('AUTHORS.rst')
+        remove_project_file('docs/authors.rst')
 
     if '{{ cookiecutter.use_pytest }}' == 'y':
         init_file = os.path.join(project_path, 'tests', '__init__.py')
-        remove_file(init_file)
+        remove_project_file(init_file)
 
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         cli_file = os.path.join(package_path, 'cli.py')
-        remove_file(cli_file)
+        remove_package_file(cli_file)
 
     if 'Other/Proprietary License' == '{{ cookiecutter.license }}':
         remove_project_file('LICENSE')
