f691fd39f1b42e6901cab241af5d6ab0f1cd741d
veit <kontakt@veit-schiele.de>
2019-11-26 10:50:27 +0100

    Exclude test_make_help on windows

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -134,8 +134,13 @@ def test_bake_without_author_file(cookies):
 
 def test_make_help(cookies):
     with bake_in_temp_dir(cookies) as result:
-        output = check_output_inside_dir('make help', str(result.project))
-        assert b"check code coverage quickly with the default Python" in output
+        if sys.platform != "win32":
+            output = check_output_inside_dir(
+                'make help',
+                str(result.project)
+            )
+            assert b"check code coverage quickly with the default Python" in \
+                output
 
 
 def test_bake_selecting_license(cookies):
