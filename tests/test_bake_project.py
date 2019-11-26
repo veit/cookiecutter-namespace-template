27bc9980928c1ca91e03d4526f2b8ee86d5a1eb0
veit <kontakt@veit-schiele.de>
2019-11-26 17:29:14 +0100

    Change python_boilerplate to vsc

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -81,7 +81,7 @@ def test_bake_with_defaults(cookies):
 
         found_toplevel_files = [f.basename for f in result.project.listdir()]
         assert 'setup.py' in found_toplevel_files
-        assert 'python_boilerplate' in found_toplevel_files
+        assert 'vsc' in found_toplevel_files
         assert 'tox.ini' in found_toplevel_files
         assert 'tests' in found_toplevel_files
 
